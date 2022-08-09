25ad1b3ff80f719e1089c664f0c2effdea61dd94
Shirshak Kandel <shirshakkandel@gmail.com>
2022-08-09 17:58:59 +0545

    next file added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aa71236c2f9f418c"; }
+/******/ 		__webpack_require__.h = function() { return "b97d84da53e6d575"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.50d43895039c0a28.hot-update.js
+++ .next/static/webpack/webpack.50d43895039c0a28.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "375c8a232bd3b4bf"; }
+/******/ 	__webpack_require__.h = function() { return "b97d84da53e6d575"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
